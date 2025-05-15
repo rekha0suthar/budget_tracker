1e123484d391581d6a121548a2949499b41bd408
rekha0suthar <rekha0suthar@gmail.com>
2025-05-15 16:11:28 +0530

    deployed frontend origin added to cors

--- backend/budjet_tracker/settings.py
+++ backend/budjet_tracker/settings.py
@@ -61,7 +61,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://budget-tracker-dusky-phi.vercel.app"
 ]
 
 
